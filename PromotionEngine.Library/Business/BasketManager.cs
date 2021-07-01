d723f33a9c66ce3c7de39effb768aa24195a47d3
muhammedfatih <mfatihinanc@gmail.com>
2021-07-01 06:35:31 +0300

    Basket manager is created.

---
+++ PromotionEngine.Library/Business/BasketManager.cs
@@ -0,0 +1,17 @@
+﻿using PromotionEngine.Library.Models;
+
+namespace PromotionEngine.Library.Business
+{
+    public class BasketManager
+    {
+        public Basket Basket { get; set; }
+        public BasketManager()
+        {
+            Basket = new Basket();
+        }
+        public double Add(BasketProductItem basketProductItem)
+        {
+            return 0.0;
+        }
+    }
+}
